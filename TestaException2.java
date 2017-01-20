88e403f31bd2b96b3819f42c357213655b7df986
Akijunior <suitsu19@gmail.com>
2017-01-20 16:02:55 -0300

    Create TestaException2.java

---
+++ TestaException2.java
@@ -0,0 +1,13 @@
+package Utils;
+
+import Principal.ClasseA;
+
+public class TestaException2 {
+	
+	public static void main(String[] args) {
+		
+		ClasseA c = new ClasseA();
+		c.metodo1();
+	}
+
+}
